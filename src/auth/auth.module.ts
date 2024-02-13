e5c07e4dd27d2419fb53cfd4cb5286a9377de1d6
avivf <avivfg8@gmail.com>
2024-02-13 09:44:25 +0200

    added controller and service (provider) to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
-@Module({}) // decorator
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService]
+})
 export class AuthModule {}
